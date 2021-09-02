f4e5f76475c1a31c6bbb2a7c732e628da1b2161d
avo.koppas@moduulo.com-at-170254356415 <avo.koppas@moduulo.com-at-170254356415>
2021-09-02 10:50:14 +0300

    Initial commit

---
+++ app/src/main/java/com/testdevlab/mymemorygame/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.testdevlab.mymemorygame.utils
+
+import com.testdevlab.mymemorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_coronavirus_24,
+    R.drawable.ic_flag_24,
+    R.drawable.ic_heart_24,
+    R.drawable.ic_key_24,
+    R.drawable.ic_shuttle_24,
+    R.drawable.ic_smoking_24,
+    R.drawable.ic_soccer_24,
+    R.drawable.ic_sports_24,
+    R.drawable.ic_star_24,
+    R.drawable.ic_subway_24,
+    R.drawable.ic_synagogue_24,
+    R.drawable.ic_wine_24,
+)
